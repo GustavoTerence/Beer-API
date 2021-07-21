b590e957d99fdb1a3ea5c80dea3b101a10becd2e
Gustavo Terence <gustavosantosterence@gmail.com>
2021-07-21 17:20:04 -0300

    novo

---
+++ src/main/java/digital/Innovation/beerstock/mapper/BeerMapperImpl.java
@@ -0,0 +1,16 @@
+package digital.Innovation.beerstock.mapper;
+
+import digital.Innovation.beerstock.dto.BeerDTO;
+import digital.Innovation.beerstock.entity.Beer;
+
+public class BeerMapperImpl implements BeerMapper {
+    @Override
+    public Beer toModel(BeerDTO beerDTO) {
+        return null;
+    }
+
+    @Override
+    public BeerDTO toDTO(Beer beer) {
+        return null;
+    }
+}
